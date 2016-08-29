1f3e3505c8ac76051e121cb1e08945536ecc5574
Simon Ng <simonng@appcoda.com>
2016-08-29 13:54:28 +0800

    Initial version

--- layers/ViewController.swift
+++ layers/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var box: UIView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Creating rounder corners
+        box.layer.cornerRadius = 10
+        
+        // Adding shadow effects
+        box.layer.shadowOffset = CGSizeMake(5, 5)
+        box.layer.shadowOpacity = 0.7
+        box.layer.shadowRadius = 5
+        box.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).CGColor
+        
+        // Applying borders
+        box.layer.borderColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).CGColor
+        box.layer.borderWidth = 3
+        
+        // Display images
+        box.layer.contents = UIImage(named: "tree.jpg")?.CGImage
+        box.layer.contentsGravity = kCAGravityResize
+        box.layer.masksToBounds = true
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
